d0402116998f11405ee0b2d5407e1fb7e45514cb
CODIFY <frankmwangi8118@gmail.com>
2025-04-20 01:42:04 +0300

    fix: fixed the mini-nav
    feat: structured the building grid

--- src/Pages/ClientDashboard/Component/Dashboard/Dashboards.jsx
+++ src/Pages/ClientDashboard/Component/Dashboard/Dashboards.jsx
@@ -2,7 +2,41 @@ import PropertyManagement from "../../../../assets/propertyManagement.png"
 import Review from "../../../../assets/review.png"
 import Pending from "../../../../assets/pending.gif"
 import "./Dashboard.css"
+import House from "../../../../assets/landing.jpg"
+
 const Dashboards = () => {
+    const buildingData = [
+        {
+            "image": House,
+            "buildingName": "mwas apartments",
+            "location": "lumumba drive",
+            "addedOn": "12.12.2025"
+        },
+        {
+            "image": House,
+            "buildingName": "mwas apartments",
+            "location": "lumumba drive",
+            "addedOn": "12.12.2025"
+        },
+        {
+            "image": House,
+            "buildingName": "mwas apartments",
+            "location": "lumumba drive",
+            "addedOn": "12.12.2025"
+        },
+        {
+            "image": House,
+            "buildingName": "mwas apartments",
+            "location": "lumumba drive",
+            "addedOn": "12.12.2025"
+        },
+        {
+            "image": House,
+            "buildingName": "mwas apartments",
+            "location": "lumumba drive",
+            "addedOn": "12.12.2025"
+        }
+    ]
     const data = [
         {
             "icon": PropertyManagement,
@@ -27,7 +61,7 @@ const Dashboards = () => {
     ]
     return (
         <div className={"Wrapper"}>
-            <div className={"card-row"}>
+            <div className={"card-rows"}>
                 {data.map((item, index) => (
                     <div key={index} className={"top-icons"}>
                         <div className={"icon"}>
@@ -40,6 +74,40 @@ const Dashboards = () => {
                     </div>
                 ))}
             </div>
+            <div className={"__nav"}>
+                <div className={"__subs"}>
+                    <ul className={"mine"}>
+                    <li className={"desc"}>DESCRIPTION</li>
+                    <li className={"pending"}>PENDING</li>
+                    <li className={"action"}>ACTION</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={"grid-Wrapper"}>
+
+                {buildingData.map((item, idx) => (
+                    <div key={idx} className={"data-grid"}>
+                        <div className={"data-left"}>
+                            <div className={"building-image"}>
+                                <img src={item.image} alt={"house"}/>
+                            </div>
+                            <div className={"data-right"}>
+                                <p className={"__name"}>{item.buildingName}</p>
+                                <p className={"__location"}>{item.location}</p>
+                                <p className={"__added"}>{item.addedOn}</p>
+                            </div>
+                            <div className={"pending-units-desc"}>
+                                <p className={"name"}>name</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+
+
         </div>
 
 
